ae7f9f68ee8dd0d08bab777328c9231907df2deb
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2024-09-08 15:49:39 +0600

    arrival card design done

---
+++ my-app/src/app/Components/Cards/ArrivalCard/ArrivalCard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Heading from "../../Tags/Heading/Heading";
+import Paragraph from "../../Tags/Paragraph/Paragraph";
+import Button from "../../Tags/Button/Button";
+import Img from "../../Tags/Img/Image";
+import { FaHeart } from "react-icons/fa";
+import { IoGitCompareOutline } from "react-icons/io5";
+import { IoCart } from "react-icons/io5";
+
+const ArrivalCard = ({ imgSrc, headingTxt, subheading, paraTxt, btnTxt }) => {
+  return (
+    <div className="flex flex-col gap-y-6 w-[370px]   ">
+      <div className="relative h-[370px] w-[370px] group   ">
+        <Img src={imgSrc} alt={"not found"} className={"h-full w-full"} />
+        <Button
+          text={btnTxt ? btnTxt : "new"}
+          className={
+            "common-btn w-[92px] h-[35px] capitalize absolute top-0 left-0 mt-[20px] ml-[20px] "
+          }
+        />
+        <div className="  absolute w-full py-[25px] bg-white bottom-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col gap-y-[21px] items-end pr-[30px] ">
+          <div className="flex flex-row gap-x-[15px] right-0 items-center ">
+            <Paragraph
+              text={"Add to Wish List"}
+              className={"text-right para-four"}
+            />
+            <FaHeart className=" [&svg]:h-[12] [&svg]:w-[14px] " />
+          </div>
+          <div className="flex flex-row gap-x-[15px] right-0 items-center ">
+            <Paragraph text={"Compare"} className={"text-right para-four"} />
+            <IoGitCompareOutline className=" [&svg]:h-[12] [&svg]:w-[12px] " />
+          </div>
+          <div className="flex flex-row gap-x-[15px] right-0 items-center ">
+            <Paragraph
+              text={"Add to Wish List"}
+              className={"text-right heding-six"}
+            />
+            <IoCart className=" [&svg]:h-[12] [&svg]:w-[13.5px] " />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-[15px]">
+        <div className="flex flex-row gap-x-[115px] ">
+          <Heading
+            text={headingTxt ? headingTxt : "Basic Crew Neck Tee"}
+            Variant={"h4"}
+            className={"heading-four"}
+          />
+          <Paragraph
+            className={"para-three"}
+            text={subheading ? subheading : "$44.00"}
+          />{" "}
+        </div>
+        <Paragraph
+          text={paraTxt ? paraTxt : "Black"}
+          className={"para-three"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ArrivalCard;
--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,6 +1,8 @@
 import NotFoundPage from "@/Pages/NotFoundPage/NotFoundPage";
 import Footer from "./Components/Layout/Footer/Footer";
 import Navbar from "./Components/Layout/Navbar/Navbar";
+import ArrivalCard from "./Components/Cards/ArrivalCard/ArrivalCard";
+import basket from "../Images/basket.png";
 
 export default function Home() {
   return (
